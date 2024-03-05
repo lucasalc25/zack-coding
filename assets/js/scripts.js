eaddab7e0aca85445b24f2b599c59c8ae4a306fa
lucasalc25 <lucas.alc25@gmail.com>
2024-03-05 12:56:36 -0400

    V2 0.2.1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const character = document.getElementById('character');
   const speechBubble = document.getElementById('speech-bubble');
   const fases = [
-    { background: '/assets/img/sala.jpg', falas: ["E aí, beleza? Bem-vindo(a) ao Coding Life!", "Seguinte, meus pais precisaram fazer uma viagem e eu vou precisar de ajuda com algumas tarefas que eles deixaram pra mim. To contando contigo pra me dar uma força hein! Vamo lá?"] },
-    { background: '/assets/img/sala.jpg', falas: ["A primeira tarefa é levar o lixo pra fora. Eu vou te dar um algoritmo todo bagunçado, você só precisa colocar os passos na ordem correta, beleza?", "Caso não saiba, um algoritmo é nada mais que um conjunto de ações que depois de serem executadas deverão resolver algum problema.", "Agora é com você, manda ver!"] }
+    { background: '/assets/img/sala.jpg', falas: ["E aí, beleza? Bem-vindo(a) ao Coding Life!", "Seguinte, meus pais precisaram fazer uma viagem e eu vou precisar de ajuda com algumas tarefas que eles me deixaram. Tô contando com você pra me dar uma força hein! Vamo lá?"] },
+    { background: '/assets/img/cozinha.jpg', falas: ["A primeira tarefa é levar o lixo pra fora. Eu vou te dar um algoritmo todo bagunçado, você só precisa colocar os passos na ordem correta, beleza?", "Caso não saiba, um algoritmo é nada mais que um conjunto de ações que depois de serem executadas deverão resolver algum problema.", "Agora é com você, manda ver!"] }
   ]
   const texto = document.getElementById('text');
+  const gameContainerElemento = document.getElementById('game-container');
 
   let indiceFaseAtual = 0;
   let faseAtual = fases[indiceFaseAtual];
@@ -27,43 +28,32 @@ document.addEventListener('DOMContentLoaded', function() {
     }
    }
 
-  // Adicionar evento de clique à tela
-  document.addEventListener('click', function() {
-    // Se a animação estiver ativa, interrompa-a e exiba o próximo texto da fase
-    if (animacaoAtiva) {
-      terminarAnimacao();
-      setTimeout(function() {
-        if (indiceTextoAtual < faseAtual.textos.length - 1) {
-          indiceTextoAtual++;
-          textoAtual = faseAtual.textos[indiceTextoAtual];
-          textoElemento.textContent = ''; // Limpa o texto atual
-          exibirTextoGradualmente(textoAtual, 0);
-          animacaoAtiva = true; // Restaura a animação
-        } else {
-          // Se todos os textos da fase foram exibidos, avance para a próxima fase
-          if (indiceFaseAtual < fases.length - 1) {
-            indiceFaseAtual++;
-            faseAtual = fases[indiceFaseAtual];
-            indiceTextoAtual = 0;
-            textoAtual = faseAtual.textos[indiceTextoAtual];
-            gameContainerElemento.style.backgroundImage = 'url(' + faseAtual.background + ')';
-            textoElemento.textContent = ''; // Limpa o texto atual
-            exibirTextoGradualmente(textoAtual, 0);
-            animacaoAtiva = true; // Restaura a animação
-          }
-        }
-      }, 500); // Tempo de espera antes de exibir o próximo texto (em milissegundos)
+  // Função para avançar para o próximo texto ou fase
+  function proximoTextoOuFase() {
+    if (animacaoAtiva) return; // Se a animação estiver ativa, não fazer nada
+    if (indiceFalaAtual < faseAtual.falas.length - 1) {
+      // Se houver mais textos na fase, avançar para o próximo texto
+      indiceFalaAtual++;
+      falaAtual = faseAtual.falas[indiceFalaAtual];
+      texto.textContent = ''; // Limpar o texto atual
+      exibirTextoGradualmente(falaAtual, 0);
+    } else {
+      // Se todos os textos da fase foram exibidos, avançar para a próxima fase
+      if (indiceFaseAtual < fases.length - 1) {
+        indiceFaseAtual++;
+        faseAtual = fases[indiceFaseAtual];
+        indiceFalaAtual = 0;
+        falaAtual = faseAtual.falas[indiceFalaAtual];
+        gameContainerElemento.style.backgroundImage = 'url(' + faseAtual.background + ')';
+        gameContainerElemento.style.backgroundSize = 'cover';
+        texto.textContent = ''; // Limpar o texto atual
+        exibirTextoGradualmente(falaAtual, 0);
+      }
     }
-  });
-
-   // Função para terminar a animação ao clicar na tela
-  function terminarAnimacao() {
-    animacaoAtiva = false;
-    texto.textContent = falas; // Exibe todo o texto instantaneamente
   }
 
-   // Adicionar evento de clique à tela
-   document.addEventListener('click', terminarAnimacao);
+  // Adicionar evento de clique à tela
+  document.addEventListener('click', proximoTextoOuFase);
 
   character.addEventListener('animationend', function() {
    // Exibir o balão de fala após a animação do personagem
